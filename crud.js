4ef1cef3aa786a099d12d0f1f0e3e2468a64f12e
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-18 14:25:07 -0700

    UPDATE: the file  with the new algorithm, this get the values and can add into the array this data

---
+++ crud.js
@@ -0,0 +1,55 @@
+class crud {
+    constructor() {
+        this.items = {};
+        this.front = 0;
+        this.end = 0;
+    }
+
+    enqueue(data) {
+        this.items[this.end] = data;
+        this.end++;
+    }
+
+    dequeue() {
+        if (this.front === this.end) {
+            return null;
+        }
+
+        const data = this.items[this.front];
+        delete this.items[this.front];
+        this.front++;
+        return data;
+    }
+
+    getSize() {
+        return this.end - this.front;
+    }
+
+    isEmpty() {
+        return this.getSize() === 0;
+    }
+
+    peek() {
+        if (this.getSize() === 0) return null;
+        return this.items[this.front];
+    }
+
+    print() {
+        if (this.getSize() === 0) return null;
+        let result = '';
+
+        for (let i = this.front; i < this.end; i++) {
+            result += this.items[i]+' ';
+        }
+
+        return result;
+    }
+}
+
+const queue = new crud();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue);
--- data_structure.js
+++ data_structure.js
@@ -66,12 +66,13 @@ function getNthFiboBetter(n, lastlast, last) {
 let n = (prompt('Ingrese un numero entero:'));
 function sieveEratosthenes(n) {
     var arreglo = []
-    limiteSuperior = Math.sqrt(n), output = [];
+    limiteSuperior = Math.sqrt(n)
+    var output = [];
 
     for (var i = 0; i < n; i++) {
         arreglo.push(true);//Inicializamos el arreglo para los numeros en true 
     }
-    
+
     for (var i = 2; i <= limiteSuperior; i++) {
         if (arreglo[i]) {
             for (var j = i * i; j < n; j += i) {
@@ -79,6 +80,7 @@ function sieveEratosthenes(n) {
             }
         }
     }
+
     for (var i = 2; i < n; i++) {
         if (arreglo[i]) {
             output.push(i);
