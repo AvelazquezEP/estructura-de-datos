15a8094d10a9011a6d81d914f3e206f266a173ef
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-18 13:49:09 -0700

    UPDATE: the new functions, new way to make some process

--- callStack.js
+++ callStack.js
@@ -1,11 +1,15 @@
 // FUNCTIONS DATA STRUCTURE
+
+/* #region printNRecursive */
 function printNRecursive(n) {
     if (n > 1) {
         printNRecursive(n - 1);
     }
     console.log(n);
 }
+/* #endregion */
 
+/* #region countDownToZero */
 function countDownToZero(n) {
     // base case. Stop at 0
     if (n < 0) {
@@ -15,7 +19,9 @@ function countDownToZero(n) {
         countDownToZero(n - 1); // count down 1
     }
 }
+/* #endregion */
 
+/* #region getNthFibo */
 function getNthFibo(n) {
     if (n <= 1) return n;
     var sum = 0,
@@ -28,10 +34,63 @@ function getNthFibo(n) {
         last = sum;
     }
     console.log(sum)
-    // return sum;
+}
+/* #endregion */
+
+/* #region getNthFibo_recursive */
+// Fibonacci Recursive
+// Time O(2^n)
+function getNthFibo_recursive(n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        // return getNthFibo_recursive(n - 1) + getNthFibo_recursive(n - 2);
+        console.log(getNthFibo_recursive(n - 1) + getNthFibo_recursive(n - 2));
+    }
+}
+/* #endregion */
+
+/* #region getNthiboBetter */
+function getNthFiboBetter(n, lastlast, last) {
+    if (n == 0) {
+        return lastlast;
+    }
+    if (n == 1) {
+        return last;
+    }
+    return getNthFiboBetter(n - 1, last, lastlast + last);
+}
+/* #endregion */
+
+
+let n = (prompt('Ingrese un numero entero:'));
+function sieveEratosthenes(n) {
+    var arreglo = []
+    limiteSuperior = Math.sqrt(n), output = [];
+
+    for (var i = 0; i < n; i++) {
+        arreglo.push(true);//Inicializamos el arreglo para los numeros en true 
+    }
+    
+    for (var i = 2; i <= limiteSuperior; i++) {
+        if (arreglo[i]) {
+            for (var j = i * i; j < n; j += i) {
+                arreglo[j] = false;
+            }
+        }
+    }
+    for (var i = 2; i < n; i++) {
+        if (arreglo[i]) {
+            output.push(i);
+        }
+    }
+    return output;
 }
 
 // printNRecursive(10);
-countDownToZero(12);
-// getNthFibo(3);
+// countDownToZero(12);
+// getNthFibo(10);
+// getNthFibo_recursive(10);
+// getNthFiboBetter(10, 1, );
+console.log(sieveEratosthenes(n));
 
